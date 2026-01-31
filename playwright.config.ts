e538932c5cb81785e83dc41cc5d1b1c2801d7d81
Ronaldo Garcia <ronaldogarcia09@gmail.com>
2026-01-30 20:58:17 -0600

    fix: Limpieza y correcciones

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
     //trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
-    headless: false,
+    headless: true,
   },
 
   /* Configure projects for major browsers */
